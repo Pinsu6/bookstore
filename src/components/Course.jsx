ce587584cdb11d1d5c84d817b42a0681111c0136
Prince <124670812+Pinsu6@users.noreply.github.com>
2024-06-05 14:31:07 +0530

    filter work done

--- src/components/Course.jsx
+++ src/components/Course.jsx
@@ -1,21 +1,37 @@
 import { useEffect, useState } from "react";
-
+import { CiFilter } from "react-icons/ci";
 import axios from "axios";
 import Cards from "./Cards";
 function Course() {
   const [book, setBook] = useState([]);
-  useEffect(() => {
-    const getBook = async () => {
-      try {
-        const response = await axios.get("http://localhost:4000/book/");
+  const [filter, setFilter] = useState("");
 
+  const getBook = async () => {
+    try {
+      const response = await axios.get("http://localhost:4000/book/");
+      setBook(response.data);
+      const freebook = response.data.filter((data) => data.category === "free");
+      const paidbook = response.data.filter((data) => data.category === "paid");
+
+      if (filter === "AllBook") {
         setBook(response.data);
-      } catch (error) {
-        console.log("error from course", error);
+      } else if (filter === "free") {
+        setBook(freebook);
+      } else if (filter === "paid") {
+        setBook(paidbook);
       }
-    };
+    } catch (error) {
+      console.log("error from course", error);
+    }
+  };
+
+  const handleChange = (event) => {
+    setFilter(event.target.value);
+  };
+
+  useEffect(() => {
     getBook();
-  }, []);
+  }, [filter]);
   return (
     <>
       <div className="max-w-screen-2xl containern mx-auto md:px-20 px-4 ">
@@ -31,6 +47,22 @@ function Course() {
             dignissimos accusamus perferendis accusantium iste!
           </p>
         </div>
+        <div className="flex flow-root">
+          <select
+            className="float-right mt-3 select  select-xs  max-w-xs"
+            value={filter}
+            onChange={handleChange}
+          >
+            <option>AllBook</option>
+            <option>free</option>
+            <option>paid</option>
+          </select>
+          <span className="mt-4 float-right">
+            <CiFilter />
+          </span>
+        </div>
+        <hr />
+
         <div className="mt-12 grid grid-cols-1 md:grid-cols-3">
           {book.map((item) => (
             <Cards key={item.id} items={item} />
